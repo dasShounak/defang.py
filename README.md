949bb525c7c87f3a7d8901d4fec34e7db2ff994e
Shounak Das <shounakdas@protonmail.com>
2025-07-12 17:42:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## defang.py
+
+A Python script to defang URLs, email  and IP addresses.
+
+```sh
+usage: defang.py [-h] -m {url,email,ip} input
+
+positional arguments:
+  input                 The string to be defanged.
+
+options:
+  -h, --help            show this help message and exit
+  -m {url,email,ip}, --mode {url,email,ip}
+                        The type of string input. Possible options are url,
+                        email and ip.
+```
---
+++ defang.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import argparse
+
+def Defang(mode, user_input):
+    defanged_string = ""
+
+    try:
+        if mode == "url":
+            user_input = user_input.replace("http", "hxxp")
+            user_input = user_input.replace("ftp", "fxp")
+            user_input = user_input.replace("://", "[://]")
+
+            for i in user_input:
+                if i == '.':
+                    defanged_string += "[.]"
+                else:
+                    defanged_string += i
+
+        elif mode == "email":
+            user_input = user_input.replace("@", "[AT]")
+
+            for i in user_input:
+                if i == '.':
+                    defanged_string += "[.]"
+                else:
+                    defanged_string += i
+
+        elif mode == "ip":
+            for i in user_input:
+                if i == '.':
+                    defanged_string += "[.]"
+                else:
+                    defanged_string += i
+    except:
+        print("Encounterd exception. Please try again. Use -h to read usage instructions.")
+
+    print(f"{defanged_string}")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    # -m MODE
+    parser.add_argument("-m", "--mode", dest = "mode", help = "The type of string input. Possible options are url, email and ip.", type = str, required = True, choices = ["url", "email", "ip"])
+    parser.add_argument("input", help = "The string to be defanged.", type = str)
+    
+    args = parser.parse_args()
+
+    user_input = args.input
+
+    Defang(args.mode, user_input)
