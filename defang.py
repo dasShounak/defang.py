9ad206bdf728b15d8b38eb9a226ee1632ac45ee2
Shounak Das <shounakdas@protonmail.com>
2025-07-18 12:40:12 +0000

    Code clean

--- defang.py
+++ defang.py
@@ -5,13 +5,15 @@
 import argparse
 
 def Defang(mode, user_input):
+
     defanged_string = ""
 
     try:
         if mode == "url":
-            user_input = user_input.replace("http", "hxxp")
-            user_input = user_input.replace("ftp", "fxp")
-            user_input = user_input.replace("://", "[://]")
+            replacements = { "http": "hxxp", "ftp": "fxp", "://": "[://]" }
+
+            for old, new in replacements.items():
+                user_input = user_input.replace(old, new)
 
             for i in user_input:
                 if i == '.':
@@ -37,17 +39,19 @@ def Defang(mode, user_input):
     except:
         print("Encounterd exception. Please try again. Use -h to read usage instructions.")
 
-    print(f"{defanged_string}")
+    print(defanged_string)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-
-    # -m MODE
-    parser.add_argument("-m", "--mode", dest = "mode", help = "The type of string input. Possible options are url, email and ip.", type = str, required = True, choices = ["url", "email", "ip"])
+    parser.add_argument("-m", "--mode",
+                        dest = "mode",
+                        help = "The type of string input. Possible options are url, email and ip.",
+                        type = str,
+                        required = True,
+                        choices = ["url", "email", "ip"]
+                        )
     parser.add_argument("input", help = "The string to be defanged.", type = str)
-    
     args = parser.parse_args()
-
 
     Defang(args.mode, args.input)
